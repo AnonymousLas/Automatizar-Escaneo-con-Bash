f9082c36b79fe3c0003914969b3a25c949b9c9ae
Marco Antonio FM * <162050547+Marco-Anonimous@users.noreply.github.com>
2024-11-20 14:39:54 -0500

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,14 +1,15 @@
--------------------------------------------------
-└─$ extractPorts allPorts1 
-[+] Extracting information... 
-        [+] IP Address: 192.168.1.44
-        [+] Open ports: 22,80,443,16667
-[+] Ports have been copied to clipboard! 
--------------------------------------------------
-
-
-
 nano ~/.zshrc
+
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
 
 
 function extractPorts() {
